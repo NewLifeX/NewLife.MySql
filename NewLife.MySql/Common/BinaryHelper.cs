6e49599cbe6ddaafbcfd50f08d61f4053f4a48d2
大石头 <nnhy@vip.qq.com>
2018-06-10 23:55:17 +0800

    登录时建立Tcp连接，成功拿到欢迎包，读取得到服务器版本和特性

---
+++ NewLife.MySql/Common/BinaryHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Data;
+
+namespace NewLife.MySql.Common
+{
+    static class BinaryHelper
+    {
+        /// <summary>读取零结尾的C格式字符串</summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static Packet ReadZero(this BinaryReader reader)
+        {
+            var ms = reader.BaseStream as MemoryStream;
+            var p = (Int32)ms.Position;
+            var buf = ms.GetBuffer();
+            var k = 0;
+            for (k = p; k < ms.Length; k++)
+            {
+                if (buf[k] == 0) break;
+            }
+
+            var len = k - p;
+            ms.Seek(len + 1, SeekOrigin.Current);
+
+            return new Packet(buf, p, len);
+        }
+
+        /// <summary>读取零结尾的C格式字符串</summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static String ReadZeroString(this BinaryReader reader) => reader.ReadZero().ToStr();
+    }
+}
--- NewLife.MySql/Driver.cs
+++ NewLife.MySql/Driver.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using NewLife.Data;
+using NewLife.MySql.Common;
 
 namespace NewLife.MySql
 {
@@ -50,6 +52,7 @@ namespace NewLife.MySql
             var msTimeout = Setting.ConnectionTimeout;
             if (msTimeout <= 0) msTimeout = 15000;
 
+            // 连接网络
             var client = new TcpClient
             {
                 ReceiveTimeout = msTimeout
@@ -57,6 +60,42 @@ namespace NewLife.MySql
             client.Connect(server, port);
 
             _Stream = client.GetStream();
+
+            // 读取数据包
+            var pk = ReadPacket();
+            var ms = pk.GetStream();
+            var reader = new BinaryReader(ms);
+
+            // 欢迎包
+            var protocol = ms.ReadByte();
+            var version = reader.ReadZeroString();
+            var threadId = reader.ReadInt32();
+
+            var seedPart1 = reader.ReadZero();
+
+            // 服务器特性
+            var capabilities = (UInt32)reader.ReadUInt16();
+            var charSet = reader.ReadByte();
+            var serverStatus = reader.ReadUInt16();
+            capabilities |= ((UInt32)reader.ReadUInt16() << 16);
+
+            ms.Seek(11, SeekOrigin.Current);
+
+            // 加密种子
+            var seedPart2 = reader.ReadZero();
+            var ms2 = new MemoryStream();
+            seedPart1.WriteTo(ms2);
+            seedPart2.WriteTo(ms2);
+            var seed = ms2.ToArray();
+
+            // 设置连接标识
+            var flags = 0;
+            var ms3 = new MemoryStream();
+            var writer = new BinaryWriter(ms3);
+            writer.Write(flags);
+            writer.Write(0xFF_FFFF);
+            writer.Write((Byte)33); // UTF-8
+            writer.Write(new Byte[23]);
         }
 
         public void Close()
@@ -98,6 +137,47 @@ namespace NewLife.MySql
 
                 return dic;
             }
+        }
+        #endregion
+
+        #region 网络操作
+        /// <summary>读取数据包</summary>
+        /// <returns></returns>
+        private Packet ReadPacket()
+        {
+            // 3字节长度 + 1字节序列号
+            var buf = _Stream.ReadBytes(4);
+            var len = buf[0] + (buf[1] << 8) + (buf[2] << 16);
+            var seq = buf[3];
+
+            buf = _Stream.ReadBytes(len);
+            var pk = new Packet(buf);
+
+            // 错误包
+            if (buf[0] == 0xFF)
+            {
+                var code = buf.ToUInt16(1);
+                var msg = ReadString(pk.Slice(1 + 2));
+                // 前面有6字符错误码
+                if (!msg.IsNullOrEmpty() && msg[0] == '#') msg = msg.Substring(6);
+
+                throw new MySqlException(code, msg);
+            }
+
+            return pk;
+        }
+
+        private static String ReadString(Packet pk)
+        {
+            var buf = pk.Data;
+            var k = 0;
+            for (k = pk.Offset; k < pk.Count; k++)
+            {
+                if (buf[k] == 0) break;
+            }
+
+            var len = k - pk.Offset;
+            return pk.ToStr(null, 0, len);
         }
         #endregion
     }
