89bd29f07547387e944e56858608a174779e90d5
大石头 <nnhy@vip.qq.com>
2025-01-28 07:00:27 +0800

    [feat]新增单元测试项目

---
+++ UnitTest/MySqlClientFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿using NewLife.MySql;
+
+namespace UnitTest;
+
+public class MySqlClientFactoryTests
+{
+    [Fact]
+    public void CreateCommand_ShouldReturnMySqlCommand()
+    {
+        // Arrange
+        var factory = MySqlClientFactory.Instance;
+
+        // Act
+        var command = factory.CreateCommand();
+
+        // Assert
+        Assert.NotNull(command);
+        Assert.IsType<MySqlCommand>(command);
+    }
+
+    [Fact]
+    public void CreateConnection_ShouldReturnMySqlConnection()
+    {
+        // Arrange
+        var factory = MySqlClientFactory.Instance;
+
+        // Act
+        var connection = factory.CreateConnection();
+
+        // Assert
+        Assert.NotNull(connection);
+        Assert.IsType<MySqlConnection>(connection);
+    }
+
+    [Fact]
+    public void CreateParameter_ShouldReturnMySqlParameter()
+    {
+        // Arrange
+        var factory = MySqlClientFactory.Instance;
+
+        // Act
+        var parameter = factory.CreateParameter();
+
+        // Assert
+        Assert.NotNull(parameter);
+        Assert.IsType<MySqlParameter>(parameter);
+    }
+
+    [Fact]
+    public void CreateConnectionStringBuilder_ShouldReturnMySqlConnectionStringBuilder()
+    {
+        // Arrange
+        var factory = MySqlClientFactory.Instance;
+
+        // Act
+        var connectionStringBuilder = factory.CreateConnectionStringBuilder();
+
+        // Assert
+        Assert.NotNull(connectionStringBuilder);
+        Assert.IsType<MySqlConnectionStringBuilder>(connectionStringBuilder);
+    }
+}
