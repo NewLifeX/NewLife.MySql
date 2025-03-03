cca9c73f13750ccce4474d84a71142f34908ff66
智能大石头 <nnhy@vip.qq.com>
2025-03-03 23:45:51 +0800

    增加XCode相关单元测试，未全部通过

---
+++ UnitTest/DALTests.cs
@@ -0,0 +1,40 @@
+﻿using NewLife.MySql;
+using XCode.DataAccessLayer;
+
+namespace UnitTest;
+
+public class DALTests
+{
+    private static String _ConnStr = "Server=localhost;Database=sys;User Id=root;Password=root;";
+
+    static DALTests()
+    {
+        //DbFactory.Create(DatabaseType.MySql);
+        DAL.AddConnStr("mysql", _ConnStr, null, "MySql");
+    }
+
+    [Fact]
+    public void TestInit()
+    {
+        var db = DbFactory.Create(DatabaseType.MySql);
+        Assert.NotNull(db);
+        Assert.Equal(DatabaseType.MySql, db.Type);
+
+        var factory = db.Factory;
+        Assert.Equal(MySqlClientFactory.Instance, factory);
+
+        var dal = DAL.Create("mysql");
+        Assert.NotNull(dal);
+        Assert.Equal(DatabaseType.MySql, dal.DbType);
+    }
+
+    [Fact]
+    public void TestTables()
+    {
+        var dal = DAL.Create("mysql");
+
+        var tables = dal.Tables;
+        Assert.NotNull(tables);
+        Assert.NotEmpty(tables);
+    }
+}
---
+++ UnitTest/MembershipTests.cs
@@ -0,0 +1,31 @@
+﻿using NewLife.MySql;
+using XCode.DataAccessLayer;
+using XCode.Membership;
+
+namespace UnitTest;
+
+public class MembershipTests
+{
+    private static String _ConnStr = "Server=localhost;Database=membership;User Id=root;Password=root;";
+
+    static MembershipTests()
+    {
+        DAL.AddConnStr("membership", _ConnStr, null, "MySql");
+    }
+
+    [Fact]
+    public void TestInit()
+    {
+        var dal = DAL.Create("membership");
+        Assert.NotNull(dal);
+        Assert.Equal(DatabaseType.MySql, dal.DbType);
+        Assert.Equal(MySqlClientFactory.Instance, dal.Db.Factory);
+    }
+
+    [Fact]
+    public void CreateTables()
+    {
+        User.Meta.Session.InitData();
+        Role.Meta.Session.InitData();
+    }
+}
